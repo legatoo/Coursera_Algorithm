810eb3ab2efaf9cff95753efd9684010eff343ac
Steven Yang <yifan.yang9@gmail.com>
2014-03-10 08:54:31 +0800

    detect negative cycle

--- MST/BellmanFordSP.java
+++ MST/BellmanFordSP.java
@@ -22,7 +22,8 @@ public class BellmanFordSP{
 		queue.add(s);
 		marked[s] = true;
 		
-		while(!queue.isEmpty()){
+		//while (!queue.isEmpty() && !noNegetativeCycle())
+		while(!queue.isEmpty() ){
 			int v = queue.pollFirst();
 			marked[v] = false;
 			relax(dg, v);
@@ -48,7 +49,22 @@ public class BellmanFordSP{
 	}
 	
 	private void findNegativeCycle(){
-		//use dfs to find the cycle.
 		
 	}
+	
+	//an approach to detect negetative edge:
+	// Bellman Ford doesn't always work, the problem is its a single source
+	// shortest path algorithm, if the negative loop is not reachable from the
+	// source you pick, it fails. However a little change to Bellman Ford could
+	// help, instead of selecting a source vertex and initialise its distance to
+	// 0, we can initialise all the distance to 0 and then run Bellman Ford.
+	// This is equivalent to add a extra vertex s' which points to all the
+	// vertexes in the original graph with 0 weight edge. Once Bellman Ford is
+	// run on the graph and we found any vertex u and edge (u,v) that make d[u]
+	// + w[u][v] < d[v], we know there must be a negative cycle leading to u,
+	// tracking back from u in the predecessor graph and we'll find the cycle.
+	//
+	// DFS is not gonna work in any way, it's obviously not able to exhaust all
+	// possible cycles. DFS can be used to find the presence of cycle in graph,
+	// but no more.
 }
